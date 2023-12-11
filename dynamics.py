82882a66e4ccccacfd1544e378b3b84f21093231
akrotz <alexkrotz@gmail.com>
2023-12-11 15:36:38 -0600

    updated skeleton

---
+++ dynamics.py
@@ -0,0 +1,14 @@
+def run_dynamics(sim):
+    return sim
+
+
+def cfssh_dynamics(traj, sim):
+    return traj
+
+
+def fssh_dynamics(traj, sim):
+    return traj
+
+
+def mf_dynamics(traj, sim):
+    return traj
---
+++ initialize.py
@@ -0,0 +1,2 @@
+def initialize(sim): # here we compute Hq, Hqc(q,p), generator of q,p and gradient of Hqc
+    return sim
