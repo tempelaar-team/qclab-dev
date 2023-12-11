ebf9ba78253a68353a4cbb448b8aa5463f0ae8b4
akrotz <alexkrotz@gmail.com>
2023-12-11 15:36:30 -0600

    updated skeleton

---
+++ main.py
@@ -0,0 +1,25 @@
+import sys
+import time
+import numpy as np
+import dynamics
+import simulation
+import initialize
+
+if __name__ == '__main__':
+    start_time = time.time()
+    args = sys.argv[-1]
+    if not args:
+        print('Usage: python main.py [opts] input_file cluster_args')
+        sys.exit()
+    input_file = args[-2]
+    cluster_args = args[-1]
+    opt = args[0]
+    # initialize simulation object
+    sim = simulation.Simulation(input_file)
+    # initialize simulation functions
+    sim = initialize.initialize(sim)
+    # run dynamics
+    sim = dynamics.run_dynamics(sim)
+
+    end_time = time.time()
+    print('took ', np.round(end_time - start_time, 3), ' seconds.')
---
+++ simulation.py
@@ -0,0 +1,20 @@
+class Simulation:
+    def __init__(self, input_file):
+        # Define default values
+        defaults = {
+            "dynamics_method": "MF",  # which dynamics method, "MF", "FSSH", "CFSSH"
+            "num_procs": 4,  # number of processors to use
+            "num_trajs": 4  # number of trajectories to run
+        }
+        # Read input values from input_file
+        input_params = {}  # store them in input_params
+        with open(input_file) as file:
+            for line in file:
+                exec(str(line), input_params)
+        inputs = list(input_params)  # inputs is list of keys in input_params
+        for key in inputs:  # copy input values into defaults
+            defaults[key] = input_params[key]
+        # read modified defaults into object
+        self.dynamics_method = defaults['dynamics_method']
+        self.num_procs = defaults['num_procs']
+        self.num_trajs = defaults['num_trajs']
