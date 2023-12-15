5ce80fd02d267016a3dc2f3f44db07c28b5cec47
kmiyazaki <ken.miyazaki@northwestern.edu>
2023-12-15 16:32:49 -0600

    removed default system hamiltonian

--- simulation.py
+++ simulation.py
@@ -5,9 +5,6 @@ class Simulation:
             "dynamics_method": "MF",  # which dynamics method, "MF", "FSSH", "CFSSH"
             "num_procs": 4,  # number of processors to use
             "num_trajs": 4,  # number of trajectories to run
-            "sys_hamil": "holstein",  # system hamiltonian
-            "hsys_rot": False,  # rotation of system hamiltonian. Default: no rotation
-            "phonon_rot": False,  # rotation of phonon coordinates. Default: no rotation
             "qp_dist": "boltz"  # phonon coordinate sampling function. Default: thermal Boltzmann distribution
         }
         # Read input values from input_file
@@ -22,7 +19,4 @@ class Simulation:
         self.dynamics_method = defaults['dynamics_method']
         self.num_procs = defaults['num_procs']
         self.num_trajs = defaults['num_trajs']
-        self.sys_hamil = defaults['sys_hamil']
-        self.hsys_rot = defaults['hsys_rot']
-        self.phonon_rot = defaults['phonon_rot']
         self.qp_dist = defaults['qp_dist']
