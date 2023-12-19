5b63d4a81e4178a0ac34be2254e12c90c38753a2
kmiyazaki <ken.miyazaki@northwestern.edu>
2023-12-19 16:12:18 -0600

    added functions in run_dynamics

--- dynamics.py
+++ dynamics.py
@@ -1,4 +1,15 @@
 def run_dynamics(sim):
+    if sim.Hstruc == "spin_boson":
+        import spin_boson as sb
+    if sim.dynamics_method == "MF":
+        sim.update_htot = sb.update_htot
+        sim.get_phonon_force = sb.get_mf_phonon_force
+    elif sim.dynamics_method == "FSSH":
+        sim.update_hq = sb.update_hq
+        sim.get_phonon_force = sb.get_fssh_phonon_force
+        sim.get_nac_numer = sb.get_nac_numer
+    elif sim.dynamics_method == "CFSSH":
+        ...
     return sim
 
 
