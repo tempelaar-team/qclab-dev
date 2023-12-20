0361da7071d48873ae9691200db5042f41fc11f1
akrotz <alexkrotz@gmail.com>
2023-12-20 14:37:52 -0500

    added FSSH dynamics

--- auxilliary.py
+++ auxilliary.py
@@ -24,3 +24,50 @@ def rk4_c(h, psi, dt):
     k4 = (-1j * h.dot(psi + dt * k3))
     psi = psi + dt * 0.166667 * (k1 + 2 * k2 + 2 * k3 + k4)
     return psi
+
+def vec_adb_to_db(psi_adb, eigvec):
+    psi_db = np.matmul(eigvec, psi_adb)
+    return psi_db
+
+
+def vec_db_to_adb(psi_db, eigvec):
+    psi_adb = np.matmul(np.conjugate(np.transpose(eigvec)),psi_db)
+    return psi_adb
+
+@jit(nopython=True)
+def rho_0_adb_to_db(rho_0_adb, eigvec): # transforms density matrix from adb to db representation
+    rho_0_db = np.dot(np.dot(eigvec, rho_0_adb + 0.0j), np.conj(eigvec).transpose())
+    return rho_0_db
+
+
+@jit(nopython=True)
+def rho_0_db_to_adb(rho_0_db, eigvec): # transforms density matrix from db to adb representation
+    rho_0_db = np.dot(np.dot(np.conj(eigvec).transpose(), rho_0_db + 0.0j), eigvec)
+    return rho_0_db
+
+def get_dkk(evec_i, evec_j, ev_diff, sim):  # computes dkk_{ij} using sparse methods
+
+def get_dab_phase(evals, evecs, sim):
+    dabq_phase = np.ones(len(evals))
+    dabp_phase = np.ones(len(evals))
+    for i in range(len(evals)-1):
+        j = i + 1
+        evec_i = evecs[:, i]
+        evec_j = evecs[:, j]
+        eval_i = evals[i]
+        eval_j = evals[j]
+        ev_diff = eval_j - eval_i
+        plus = 0
+        if np.abs(ev_diff) < 1e-14:
+            plus = 1
+            print('Warning: Degenerate eigenvalues')
+        dkkq, dkkp = get_dkk(evec_i, evec_j, ev_diff + plus, sim)
+        dkkq_angle = np.angle(dkkq[np.argmax(np.abs(dkkq))])
+        dkkp_angle = np.angle(dkkp[np.argmax(np.abs(dkkp))])
+        if np.max(np.abs(dkkq)) < 1e-14:
+            dkkq_angle = 0
+        if np.max(np.abs(dkkp)) < 1e-14:
+            dkkp_angle = 0
+        dabq_phase[i+1:] = np.exp(1.0j * dkkq_angle) * dabq_phase[i+1:]
+        dabp_phase[i+1:] = np.exp(1.0j * dkkp_angle) * dabp_phase[i+1:]
+    return dabq_phase, dabp_phase
--- dynamics.py
+++ dynamics.py
@@ -53,6 +53,125 @@ def cfssh_dynamics(traj, sim):
 
 @ray.remote
 def fssh_dynamics(traj, sim):
+    start_time = time.time()
+    np.random.seed(traj.seed)
+    #  initialize classical coordinates
+    q, p = sim.init_classical()
+    #  compute initial Hamiltonian
+    h_q = sim.H_q()
+    h_tot = h_q + sim.h_qc(q, p)
+    #  compute eigenvectors
+    evals, evecs = np.linalg.eigh(h_tot)
+    num_states = len(h_q)
+    # compute initial gauge shift for real-valued derivative couplings
+    dab_q_phase, dab_p_phase = auxilliary.get_dab_phase(evals, evecs, sim)
+    # execute phase shift
+    evecs = np.matmul(evecs, np.diag(np.conjugate(dab_q_phase)))
+    # recalculate phases and check that they are zero
+    dab_q_phase, dab_p_phase = auxilliary.get_dab_phase(evals, evecs, sim)
+    if np.sum(np.abs(np.imag(dab_q_phase))**2 + np.abs(np.imag(dab_p_phase))**2) > 1e-10:
+        print('Warning: phase init', np.sum(np.abs(np.imag(dab_q_phase))**2 + np.abs(np.imag(dab_p_phase))**2) )
+
+    #  initial wavefunction in diabatic basis
+    psi_db = sim.psi_db_0
+    # determine initial adiabatic wavefunction in fixed gauge
+    psi_adb = auxilliary.vec_db_to_adb(psi_db, evecs)
+    # determine initial active surface
+    intervals = np.zeros(num_states)
+    for n in range(num_states):
+        intervals[n] = np.sum(np.real(np.abs(psi_adb)**2)[0:n+1])
+    rand_val = np.random.rand()
+    # initialize active surface index
+    act_surf_ind = np.arange(num_states)[intervals > rand_val][0]
+    # initialize active surface
+    act_surf = np.zeros(num_states, dtype=int)
+    act_surf[act_surf_ind] = 1
+    # initialize outputs
+    tdat = np.arange(0,sim.tmax + sim.dt, sim.dt)
+    tdat_bath = np.arange(0, sim.tmax + sim.dt_bath, sim.dt_bath)
+    pops_db = np.zeros((len(tdat), num_states))
+    ec = np.zeros((len(tdat)))
+    eq = np.zeros((len(tdat)))
+    t_ind = 0
+    hop_count = 0
+    for t_bath_ind  in np.arange(0, len(tdat_bath)):
+        if t_ind == len(tdat):
+            break
+        if tdat[t_ind] <= tdat_bath[t_bath_ind] + 0.5 * sim.dt_bath:
+            den_mat_adb = np.outer(psi_adb, np.conjugate(psi_adb))
+            den_mat_adb[range(num_states), range(num_states)] = act_surf
+            den_mat_db = auxilliary.rho_0_adb_to_db(den_mat_adb, evecs)
+            pops_db[t_ind] = np.real(np.diag(den_mat_db))
+            ec[t_ind] = sim.h_c(q, p)
+            eq[t_ind] = np.real(np.matmul(np.conjugate(psi_db), np.matmul(h_tot, psi_db)))
+            t_ind += 1
+        fq, fp = sim.quantumforce(evecs[:, act_surf_ind], sim)
+        q, p = auxilliary.rk4_c(q, p, (fq, fp), sim.w_c, sim.dt_bath)
+        evecs_previous = np.copy(evecs)
+        h_tot = h_q + sim.h_qc(q, p)
+        evals, evecs = np.linalg.eigh(h_tot)
+        evecs, evec_phases = auxilliary.sign_adjust(evecs, evecs_previous, evals, sim)
+        evals_exp = np.exp(-1j * evals * sim.dt_bath)
+        diag_matrix = np.diag(evals_exp)
+        psi_adb = np.dot(diag_matrix, psi_adb)
+        psi_db = auxilliary.vec_adb_to_db(psi_adb, evecs)
+        rand = np.random.rand()
+        prod = np.matmul(np.conj(evecs[:, act_surf_ind]), evecs_previous)
+        hop_prob = -2*np.real(prod * (psi_adb / psi_adb[act_surf_ind]))
+        hop_prob[act_surf_ind] = 0
+        bin_edge = 0
+        for k in range(len(hop_prob)):
+            hop_prob[k] = auxilliary.nan_num(hop_prob[k])
+            bin_edge = bin_edge + hop_prob[k]
+            if rand < bin_edge:
+                eig_k = evecs[:, act_surf_ind]
+                eig_j = evecs[:, k]
+                eigval_k = evecs[act_surf_ind]
+                eigval_j = evecs[k]
+                ev_diff = eigval_j - eigval_k
+                dkkq, dkkp = auxilliary.get_dkk(eig_k, eig_j, ev_diff, sim)
+                if np.abs(np.sin(np.angle(dkkq[np.argmax(np.abs(dkkq))]))) > 1e-2:
+                    print('ERROR IMAGINARY DKKQ: \n', 'angle: ',
+                          np.abs(np.sin(np.angle(dkkq[np.argmax(np.abs(dkkq))]))),
+                          '\n magnitude: ', np.abs(dkkq[np.argmax(np.abs(dkkq))]),
+                          '\n value: ', dkkq[np.argmax(np.abs(dkkq))])
+                if np.abs(np.sin(np.angle(dkkq[np.argmax(np.abs(dkkq))]))) > 1e-2:
+                    print('ERROR IMAGINARY DKKP: \n', 'angle: ',
+                          np.abs(np.sin(np.angle(dkkp[np.argmax(np.abs(dkkp))]))),
+                          '\n magnitude: ', np.abs(dkkp[np.argmax(np.abs(dkkp))]),
+                          '\n value: ', dkkp[np.argmax(np.abs(dkkp))])
+                delta_q = np.real(dkkp)
+                delta_p = np.real(dkkq)
+                akkq = np.sum((1 / 2) * np.abs(
+                    delta_p) ** 2)
+                akkp = np.sum((1 / 2) * (np.nan_to_num(sim.w_c) ** 2) * np.abs(
+                    delta_q) ** 2)
+                bkkq = np.sum((p * delta_p))
+                bkkp = -np.sum((np.nan_to_num(sim.w_c) ** 2) * q * delta_q)
+                disc = (bkkq + bkkp) ** 2 - 4 * (akkq + akkp) * ev_diff
+                if disc >= 0:
+                    if bkkq + bkkp < 0:
+                        gamma = (bkkq + bkkp) + np.sqrt(disc)
+                    else:
+                        gamma = (bkkq + bkkp) - np.sqrt(disc)
+                    if akkp + akkq == 0:
+                        gamma = 0
+                    else:
+                        gamma = gamma / (2 * (akkq + akkp))
+                    p = p - np.real(gamma) * delta_p  # rescale
+                    q = q + np.real(gamma) * delta_q  # rescale
+                    act_surf_ind = k
+                    act_surf = np.zeros_like(act_surf)
+                    act_surf[act_surf_ind] = 1
+                    hop_count += 1
+                break
+    traj.add_to_dic('pops_db', pops_db)
+    traj.add_to_dic('t', tdat)
+    traj.add_to_dic('eq', eq)
+    traj.add_to_dic('ec', ec)
+    end_time = time.time()
+    msg = 'trial index: ' + str(traj.index) + ' hop count: ' + str(hop_count) + ' time: ' + str(
+        end_time - start_time) + ' seed: ' + str(traj.seed)
     return traj, msg
 
 @ray.remte
@@ -84,7 +203,7 @@ def mf_dynamics(traj, sim):
             pops_db[t_ind] = np.abs(psi_db)**2
             t_ind += 1
         fq, fp = sim.quantum_force(psi_db)
-        q, p = auxilliary.rk4_c(q, p, (fq, fp), sim.w_list, sim.dt_bath)
+        q, p = auxilliary.rk4_c(q, p, (fq, fp), sim.w_c, sim.dt_bath)
         h_tot = h_q + sim.h_qc(q, p)
         psi_db = auxilliary.rk4_q(h_tot, psi_db, sim.dt_bath)
     # add data to trajectory object
