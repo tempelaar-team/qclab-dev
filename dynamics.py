c937126455049c0ea27ad913bcb6adea9b5d94a9
akrotz <alexkrotz@gmail.com>
2023-12-20 13:38:35 -0500

    added dynamics driver. Added Trajectory and Data classes for io.

--- dynamics.py
+++ dynamics.py
@@ -1,24 +1,59 @@
+import ray
+import time
+import dill as pickle
+import path
+import simulation
+import numpy as np
+
 def run_dynamics(sim):
-    if sim.dynamics_method == "MF":
-        return mf_dynamics(traj, sim)
-    elif sim.dynamics_method == "FSSH":
-        return fssh_dynamics(traj,sim)
-    elif sim.dynamics_method == "CFSSH":
-        return cfssh_dynamics(traj, sim)
+    start_time = time.time()
+    ray.shutdown()
+    ray.init(sim.cluster_args)
+    if sim.num_procs > sim.num_trials:
+        sim.num_procs = sim.num_trials
+    ray_sim = ray.put(sim) # put simulation object in shared memory
+    data_filename = sim.calc_dir + '/data.out' # output data object
+    if path.exists(data_filename):
+        data_file = open(data_filename, 'rb')
+        data_obj = pickle.load(data_file)
+        data_file.close()
+        # determine index of last trajectory
+        last_index = data_obj.index_list[-1] + 1
+    else:
+        # set last_index to 0 if new run
+        last_index = 0
+        # initialize data object
+        data_obj = simulation.Data(data_filename)
+    seeds = np.array([n for n in np.arange(last_index, sim.trials + last_index)])
+    for run in range(0, int(sim.num_trials / sim.num_procs)):
+        index_list = [run * sim.num_procs + i + last_index for i in range(sim.num_procs)]
+        seed_list = [seeds[run * sim.nprocs + i + last_index] for i in range(sim.nprocs)]
+        if sim.dynamics_method == "MF":
+            results = [mf_dynamics.remote(simulation.Trajectory(seed_list[i], index_list[i]), ray_sim) \
+                       for i in range(sim.num_procs)]
+        elif sim.dynamics_method == "FSSH":
+            results = [fssh_dynamics.remote(simulation.Trajectory(seed_list[i], index_list[i]), ray_sim) \
+                       for i in range(sim.num_procs)]
+        elif sim.dynamics_method == "CFSSH":
+            results = [cfssh_dynamics.remote(simulation.Trajectory(seed_list[i], index_list[i]), ray_sim) \
+                       for i in range(sim.num_procs)]
+        for r in results:
+            traj_obj, msg = ray.get(r)
+            print(msg)
+            data_obj.add_data(traj_obj)
+    data_file = open(data_obj.filename, 'wb')
+    pickle.dump(data_obj, data_file)
+    data_file.close()
     return sim
 
-
+@ray.remote
 def cfssh_dynamics(traj, sim):
-    return traj
+    return traj, msg
 
-
+@ray.remote
 def fssh_dynamics(traj, sim):
-    return traj
+    return traj, msg
 
-
+@ray.remte
 def mf_dynamics(traj, sim):
-    if sim.Hstruc == "spin_boson":
-        import spin_boson as sb
-    elif sim.Hstruc == "...":
-        ...
-    return traj
+    return traj, msg
--- simulation.py
+++ simulation.py
@@ -1,3 +1,4 @@
+import numpy as np
 class Simulation:
     def __init__(self, input_file):
         # Define default values
@@ -24,3 +25,39 @@ class Simulation:
         self.qp_dist = defaults['qp_dist']
         self.specden = defaults['specden']
         self.Hstruc = defaults['Hstruc']
+
+class Trajectory:
+    def __init__(self, seed, index):
+        self.seed = seed  # seed used to initialize random variables
+        self.index = index  # index of trajectory
+        self.data_dic = {}  # dictionary to store data
+    def add_to_dic(self, name, data):
+        self.data_dic.__setitem__(name, data)
+        return
+
+class Data:
+    def __init__(self, filename):
+        self.filename = filename
+        self.data_dic = {}
+        self.index_list = np.array([], dtype=int)
+        self.seed_list = np.array([], dtype=int)
+
+    def add_data(self, traj_obj):  # adds data from a traj_obj
+        for key, val in traj_obj.data_dic.items():
+            if key in self.data_dic:
+                self.data_dic[key] = self.data_dic[key] + val
+            else:
+                self.data_dic[key] = val
+        self.seed_list = np.append(self.seed_list, traj_obj.seed)
+        self.index_list = np.append(self.index_list, traj_obj.index)
+        return
+
+    def sum_data(self, data_obj):  # adds data from a data_obj
+        for key, val in data_obj.data_dic.items():
+            if key in self.data_dic:
+                self.data_dic[key] = self.data_dic[key] + val
+            else:
+                self.data_dic[key] = val
+        self.seed_list = np.concatenate((self.seed_list, data_obj.seed_list))
+        self.index_list = np.concatenate((self.index_list, data_obj.index_list))
+        return
