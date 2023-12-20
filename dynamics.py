5762cbe8a388165e074cea442a4439a7ce614199
akrotz <alexkrotz@gmail.com>
2023-12-20 13:54:34 -0500

    added MF dynamics

--- dynamics.py
+++ dynamics.py
@@ -4,6 +4,7 @@ import dill as pickle
 import path
 import simulation
 import numpy as np
+import auxilliary
 
 def run_dynamics(sim):
     start_time = time.time()
@@ -56,4 +57,42 @@ def fssh_dynamics(traj, sim):
 
 @ray.remte
 def mf_dynamics(traj, sim):
+    start_time = time.time()
+    np.random.seed(traj.seed)
+    #  initialize classical coordinates
+    q, p = sim.init_classical()
+
+    #  compute initial Hamiltonian
+    h_q = sim.H_q()
+    h_tot = h_q + sim.h_qc(q, p)
+    num_states = len(h_q)
+    # initial wavefunction in diabatic basis
+    psi_db = sim.psi_db_0
+    #  initialize outputs
+    tdat = np.arange(0, sim.tmax + sim.dt, sim.dt)
+    tdat_bath = np.arange(0, sim.tmax + sim.dt_bath, sim.dt_bath)
+    pops_db = np.zeros((len(tdat), num_states))  # diabatic populations
+    ec = np.zeros((len(tdat)))  # classical energy
+    eq = np.zeros((len(tdat)))  # quantum energy
+
+    t_ind = 0
+    for t_bath_ind in np.arange(0, len(tdat_bath)):
+        if t_ind == len(tdat):
+            break
+        if tdat[t_ind] <= tdat_bath[t_bath_ind] + 0.5 * sim.dt_bath:
+            ec[t_ind] = sim.H_c(q, p)
+            eq[t_ind] = np.real(np.matmul(np.conjugate(psi_db),np.matmul(h_tot, psi_db)))
+            pops_db[t_ind] = np.abs(psi_db)**2
+            t_ind += 1
+        fq, fp = sim.quantum_force(psi_db)
+        q, p = auxilliary.RK4_C(q, p, (fq, fp), sim.w_list, sim.dt_bath)
+        h_tot = h_q + sim.h_qc(q, p)
+        psi_db = auxilliary.RK4_Q(h_tot, psi_db, sim.dt_bath)
+    # add data to trajectory object
+    traj.add_to_dic('pops_db', pops_db)
+    traj.add_to_dic('t', tdat)
+    traj.add_to_dic('eq', eq)
+    traj.add_to_dic('ec', ec)
+    end_time = time.time()
+    msg = 'trial index: ', + str(traj.index) + ' time: ' + str(np.round(end_time - start_time, 3)) + ' seed: ' + str(traj.seed)
     return traj, msg
