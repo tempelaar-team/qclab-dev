3c6cf58df770d0773806971402ac99af889192f7
kmiyazaki <ken.miyazaki@northwestern.edu>
2023-12-19 18:03:14 -0600

    edited run_dynamics

--- dynamics.py
+++ dynamics.py
@@ -1,15 +1,10 @@
 def run_dynamics(sim):
-    if sim.Hstruc == "spin_boson":
-        import spin_boson as sb
     if sim.dynamics_method == "MF":
-        sim.update_htot = sb.update_htot
-        sim.get_phonon_force = sb.get_mf_phonon_force
+        return mf_dynamics(traj, sim)
     elif sim.dynamics_method == "FSSH":
-        sim.update_hq = sb.update_hq
-        sim.get_phonon_force = sb.get_fssh_phonon_force
-        sim.get_nac_numer = sb.get_nac_numer
+        return fssh_dynamics(traj,sim)
     elif sim.dynamics_method == "CFSSH":
-        ...
+        return cfssh_dynamics(traj, sim)
     return sim
 
 
@@ -22,4 +17,8 @@ def fssh_dynamics(traj, sim):
 
 
 def mf_dynamics(traj, sim):
+    if sim.Hstruc == "spin_boson":
+        import spin_boson as sb
+    elif sim.Hstruc == "...":
+        ...
     return traj
