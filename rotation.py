361e95927baa4389ea3444686a0580f1be7d78e2
akrotz <alexkrotz@gmail.com>
2023-12-21 12:12:45 -0500

    Added rotation of classical and quantum coordiantes.

--- rotation.py
+++ rotation.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 
 def rotate(sim):
     """
@@ -10,5 +10,14 @@ def rotate(sim):
     U_q = sim.U_q() # quantum rotation matrix
     U_c = sim.U_c() # classical rotation matrix
 
+    (dq_shape, dq_ind, dq_mels, dp_shape, dp_ind, dp_mels) = sim.dq_vars
+
+    # rotate h_q()
+    h_q_mat = np.copy(np.matmul(np.conjugate(np.transpose(U_q)),np.matmul(sim.h_q(), U_q)))
+    def h_q():
+        return h_q_mat
+
+
+    # reconstruct \nabla_{q} H and \nabla_{p} H tensor
 
 
