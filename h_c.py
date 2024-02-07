ab7158e7665b16d0b46cf36934c79db1bb1e488a
akrotz <alexkrotz@gmail.com>
2024-02-07 15:33:02 -0600

    added constants.py to hold physical constants

--- h_c.py
+++ h_c.py
@@ -1,4 +1,5 @@
 import numpy as np
+import constants
 
 def harmonic_oscillator(z, zc, sim):
     """
@@ -28,3 +29,15 @@ def harmonic_oscillator_dh_c_dzc(z, zc, sim):
     :return:
     """
     return sim.h*z
+
+def harmonic_oscillator_init_classical_boltzmann(sim):
+    """
+    Initialize classical coordiantes according to Boltzmann statistics
+    :param sim: simulation object with temperature, harmonic oscillator mass and frequency
+    :return: z = sqrt(w*h/2)*(q + i*(p/((w*h))), z* = sqrt(w*h/2)*(q - i*(p/((w*h)))
+    """
+    q = np.random.normal(loc=0, scale=np.sqrt(constants.kB_therm*sim.temp/(sim.m*(sim.h**2))), size=sim.num_states)
+    p = np.random.normal(loc=0, scale=np.sqrt(sim.m*constants.kB_therm*sim.temp), size=sim.num_states)
+    z = np.sqrt(sim.h * sim.m / 2) * (q + 1.0j * (p / (sim.h*sim.m)))
+    zc = np.conjugate(z)
+    return z, zc
