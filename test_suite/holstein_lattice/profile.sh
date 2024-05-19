7c67b2ee29a756d0f72f7ccf9a05e03de2260ee7
akrotz <alexkrotz@gmail.com>
2024-05-19 16:36:51 +0200

    Added profiling script.

---
+++ test_suite/holstein_lattice/profile.sh
@@ -0,0 +1,8 @@
+rm -r *_output
+script_path='/home/akrotz/Research/pyMQC/'
+echo 'Profiling Holstein lattice Mean-Field '
+python -m cProfile -o mf_profile.out $script_path/main.py holstein_lattice_input_MF '{"address":"auto"}'
+echo 'Profiling Holstein Fewest-Switches Surface Hopping Deterministic '
+python -m cProfile -o fssh_profile.out $script_path/main.py holstein_lattice_input_FSSH_deterministic '{"address":"auto"}'
+echo 'Profiling Holstein lattice Coherent Fewest-Switches Surface Hopping Deterministic '
+python -m cProfile -o cfssh_profile.out $script_path/main.py holstein_lattice_input_CFSSH_deterministic '{"address":"auto"}'
