ef882bb98969323974bb8055c4acac96661efb0c
akrotz <alexkrotz@gmail.com>
2024-06-16 19:12:21 -0500

    Added parallel and serial drivers.

---
+++ drivers/serial_driver.py
@@ -0,0 +1,9 @@
+import simulation
+import dynamics
+from tqdm import tqdm
+
+def dynamics_serial(sim, seeds, data = simulation.Data()):
+    for seed in tqdm(seeds):
+        traj = dynamics.dynamics(sim, simulation.Trajectory(seed))
+        data.add_data(traj)
+    return data
