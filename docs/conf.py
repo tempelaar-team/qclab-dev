d7a8f9031f266436e38becb6aaf7b342f943c4e4
akrotz <alexkrotz@gmail.com>
2024-07-17 10:07:17 -0500

    Updated name from qclab to QClab

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'qclab'
+project = 'QClab'
 copyright = '2024, Alex Krotz, Ken Miyazaki, Anna Bondarenko, Roel Tempelaar'
 author = 'Alex Krotz, Ken Miyazaki, Anna Bondarenko, Roel Tempelaar'
 
