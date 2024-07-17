141737d5c85de5dd6a4c4ecdaaa78c0a4a6fc1fe
akrotz <alexkrotz@gmail.com>
2024-07-17 10:07:00 -0500

    updated serial driver

--- qclab/drivers/serial_driver.py
+++ qclab/drivers/serial_driver.py
@@ -2,8 +2,8 @@ import qclab.simulation as simulation
 import qclab.dynamics as dynamics
 from tqdm import tqdm
 
-def dynamics_serial(sim, seeds, data = simulation.Data()):
+def dynamics_serial(algorithm, sim, seeds, data = simulation.Data()):
     for seed in tqdm(seeds):
-        traj = dynamics.dynamics(sim, simulation.Trajectory(seed))
+        traj = dynamics.dynamics(algorithm, sim, seed, simulation.Trajectory(seed))
         data.add_data(traj)
     return data
