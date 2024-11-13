63a0fcc08befe4db7a9ba12e89c0a1f696628e60
akrotz <alexkrotz@gmail.com>
2024-11-13 16:00:09 -0600

    updated script formatting

--- examples/ab_initio_script.py
+++ examples/ab_initio_script.py
@@ -4,36 +4,21 @@ from qclab.recipes import AbInitioMeanFieldDynamicsRecipe
 from qclab.models.ab_initio import AbInitioModel
 from pyscf import gto
 
-# Define the molecule
-mol = gto.M(
-    atom="""
-    H  0.00000000   0.00000000   0.00000000
-    H  0.00000000   0.00000000   2.00000000
-    """,
-    basis='6-31g',
-)
-
-# Define an initial momentum for each atom
-init_momentum = np.array([[0, 0, 0],
-                          [0, 0,-1]])
-
-# initialize the ab initio model providing the mol object, initial momentum, 
+# Define the molecule.
+mol = gto.M(atom=""" H  0.0   0.0   0.0\n H  0.0   0.0   2.0""",basis='6-31g')
+# Define an initial momentum for each atom.
+init_momentum = np.array([[0, 0, 0], [0, 0,-1]])
+# Initialize the ab initio model providing the mol object, initial momentum, 
 # number of adiabatic surfaces, and electronic structure theory method. 
 input_params = dict(pyscf_mol = mol, init_momentum=init_momentum, num_states=5, method='CISD')
 model = AbInitioModel(input_params)
-
-# define an initial wavefunction in the adiabatic basis
+# Define an initial wavefunction in the adiabatic basis.
 model.wf_adb = np.zeros(model.num_states)+0.0j
 model.wf_adb[0] = 1.0+0.0j
-
-# initialize the dynamics algorithm
+# Initialize the dynamics algorithm and set the simulation parameters.
 recipe = AbInitioMeanFieldDynamicsRecipe()
-
-# set the simulation parameters
 recipe.params.tmax = 4
 recipe.params.dt_output = 0.01
 recipe.params.dt = 0.01
-
-
-# execute the simulation using the dynamics driver
+# Execute the simulation using the dynamics driver
 data = dynamics_serial(recipe = recipe, model = model)
