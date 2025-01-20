2aa176894982a586ab1e8992cbeb082a2545d2d5
akrotz <alexkrotz@gmail.com>
2025-01-20 15:02:59 -0500

    changed name of parameters to constants in model class.

--- qclab/model.py
+++ qclab/model.py
@@ -2,8 +2,9 @@
 This file contains the Model class, which is the base class for Model objects in QC Lab.
 """
 
-from qclab.parameter import Parameter
-
+from qclab.parameter import Constants
+from qclab.simulation import VectorObject
+import qclab.ingredients as ingredients
 
 class Model:
     """
@@ -13,26 +14,21 @@ class Model:
         parameters (ParameterClass): The parameters of the model.
     """
 
-    def __init__(self, default_parameters = None, parameters=None):
-        """
-        Initializes the ModelClass with given parameters.
+    def __init__(self, default_constants = None, constants=None):
+        if constants is None:
+            constants = {}
+        if default_constants is None:
+            default_constants = {}
+        # Add default constants to the provided constants if not already present
+        constants = {**default_constants, **constants}
+        self.constants = Constants(self.update_model_constants)
+        for key, val in constants.items():
+            setattr(self.constants, key, val)
+        self.constants._init_complete = True
+        self.update_model_constants()
+        self.parameters = VectorObject()
 
-        Args:
-            parameters (dict): A dictionary of parameters to initialize the model.
-        """
-        if parameters is None:
-            parameters = {}
-        if default_parameters is None:
-            default_parameters = {}
-        # Add default parameters to the provided parameters if not already present
-        parameters = {**default_parameters, **parameters}
-        self.parameters = Parameter(self.update_model_parameters)
-        for key, val in parameters.items():
-            setattr(self.parameters, key, val)
-        self.parameters._init_complete = True
-        self.update_model_parameters()
-
-    def update_model_parameters(self):
+    def update_model_constants(self):
         """
         Update model parameters. This method should be overridden by subclasses.
         """
@@ -64,3 +60,9 @@ class Model:
             None
         """
         return
+
+    dh_c_dzc = ingredients.dh_c_dzc_finite_differences
+    dh_qc_dzc = ingredients.dh_qc_dzc_finite_differences
+    init_classical = ingredients.numerical_boltzmann_init_classical
+    #hop_function = ingredients.numerical_fssh_hop
+    
