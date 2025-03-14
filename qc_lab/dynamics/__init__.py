6ebc7f179974a146b45a2da74bdb8176387feb10
akrotz <alexkrotz@gmail.com>
2025-03-14 06:55:42 -0500

    updated imports in dynamics

--- qc_lab/dynamics/__init__.py
+++ qc_lab/dynamics/__init__.py
@@ -3,5 +3,6 @@ Directory for dynamics core and drivers.
 """
 
 from qc_lab.dynamics.slurm_driver import slurm_driver
+from qc_lab.dynamics.parallel_driver import *
 from qc_lab.dynamics.serial_driver import serial_driver
 from qc_lab.dynamics.dynamics import dynamics
