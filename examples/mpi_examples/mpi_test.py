2af36c9e264e0549d38bf984afe6b914bf777a89
akrotz <alexkrotz@gmail.com>
2025-03-14 07:05:29 -0500

    updated mpi example

--- examples/mpi_examples/mpi_test.py
+++ examples/mpi_examples/mpi_test.py
@@ -1,15 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpi4py import MPI
 from qc_lab import Simulation 
 from qc_lab.models import SpinBosonModel
 from qc_lab.algorithms import MeanField 
-from qc_lab.dynamics import serial_driver, mpi_driver
+from qc_lab.dynamics import parallel_driver_mpi
 
 # instantiate a simulation
 sim = Simulation()
 
 # change settings to customize simulation
-sim.settings.num_trajs =4000
+sim.settings.num_trajs = 400
 sim.settings.batch_size = 100
 sim.settings.tmax = 10
 sim.settings.dt = 0.001
@@ -21,5 +22,10 @@ sim.algorithm = MeanField()
 # define an initial diabatic wavefunction 
 sim.state.wf_db = np.array([1, 0], dtype=complex)
 
-data = mpi_driver(sim)
-print(data.data_dic['seed'])
+data = parallel_driver_mpi(sim)
+
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+if rank == 0:
+    print(data.data_dic['seed'])
