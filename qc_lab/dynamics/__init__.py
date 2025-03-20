7fc99ecb2233a00db4b5e955b62b1f17b3cd8f91
akrotz <alexkrotz@gmail.com>
2025-03-20 11:35:13 -0500

    formatting

--- qc_lab/dynamics/__init__.py
+++ qc_lab/dynamics/__init__.py
@@ -2,7 +2,9 @@
 Directory for dynamics core and drivers.
 """
 
-from qc_lab.dynamics.parallel_driver_multiprocessing import parallel_driver_multiprocessing
+from qc_lab.dynamics.parallel_driver_multiprocessing import (
+    parallel_driver_multiprocessing,
+)
 from qc_lab.dynamics.parallel_driver_mpi import parallel_driver_mpi
 from qc_lab.dynamics.serial_driver import serial_driver
 from qc_lab.dynamics.dynamics import dynamics
