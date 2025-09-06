16b2961d8ed157f7c8e936b70ff9425de209b1a4
akrotz <alexkrotz@gmail.com>
2025-09-06 17:24:07 -0500

    Added test for incommensurate batch sizes in drivers.

--- tests/test_drivers.py
+++ tests/test_drivers.py
@@ -106,6 +106,104 @@ def test_drivers_spinboson_mpi():
         print("parallel and serial results match!")
     return
 
+def test_incommensurate_batch_size_serial():
+    """
+    This test checks that the drivers work correctly when the number of trajectories
+    is not an integer multiple of the batch size.
+    """
+    import numpy as np
+    from qc_lab import Simulation  # import simulation class
+    from qc_lab.models import SpinBoson  # import model class
+    from qc_lab.algorithms import MeanField  # import algorithm class
+    from qc_lab.dynamics import (
+        serial_driver
+    )  # import dynamics driver
+
+    sim = Simulation()
+    sim.settings.progress_bar = False
+    sim.settings.num_trajs = 200  # Not an integer multiple of batch_size
+    sim.settings.batch_size = 50
+    sim.settings.tmax = 10
+    sim.settings.dt_update = 0.01
+
+    sim.model = SpinBoson(
+        {
+            "V": 0.5,
+            "E": 0.5,
+            "A": 100,
+            "W": 0.1,
+            "l_reorg": 0.005,
+            "boson_mass": 1.0,
+            "kBT": 1.0,
+        }
+    )
+    sim.algorithm = MeanField()
+    sim.model.initialize_constants()
+    sim.state.wf_db = np.zeros((sim.model.constants.num_quantum_states), dtype=complex)
+    sim.state.wf_db[0] += 1.0
+    print("Running commensurate batch size serial driver...")
+    data_serial = serial_driver(sim)
+    sim.settings.batch_size = 30 # Change batch size to not divide num_trajs.
+    print("Running incommensurate batch size serial driver...")
+    data_serial_incomm = serial_driver(sim)
+    print("Comparing results...")
+    for key, val in data_serial.data_dict.items():
+        if isinstance(val, np.ndarray):
+            assert np.allclose(val, data_serial_incomm.data_dict[key])
+            # assert np.allclose(val, data_parallel_mpi.data_dict[key])
+    print("results match!")
+    return
+
+def test_incommensurate_batch_size_multiprocessing():
+    """
+    This test checks that the drivers work correctly when the number of trajectories
+    is not an integer multiple of the batch size.
+    """
+    import numpy as np
+    from qc_lab import Simulation  # import simulation class
+    from qc_lab.models import SpinBoson  # import model class
+    from qc_lab.algorithms import MeanField  # import algorithm class
+    from qc_lab.dynamics import (
+        parallel_driver_multiprocessing,
+    )  # import dynamics driver
+
+    sim = Simulation()
+    sim.settings.progress_bar = False
+    sim.settings.num_trajs = 200  # Not an integer multiple of batch_size
+    sim.settings.batch_size = 50
+    sim.settings.tmax = 10
+    sim.settings.dt_update = 0.01
+
+    sim.model = SpinBoson(
+        {
+            "V": 0.5,
+            "E": 0.5,
+            "A": 100,
+            "W": 0.1,
+            "l_reorg": 0.005,
+            "boson_mass": 1.0,
+            "kBT": 1.0,
+        }
+    )
+    sim.algorithm = MeanField()
+    sim.model.initialize_constants()
+    sim.state.wf_db = np.zeros((sim.model.constants.num_quantum_states), dtype=complex)
+    sim.state.wf_db[0] += 1.0
+    print("Running commensurate batch size parallel driver...")
+    data_parallel = parallel_driver_multiprocessing(sim)
+    sim.settings.batch_size = 30 # Change batch size to not divide num_trajs.
+    print("Running incommensurate batch size parallel driver...")
+    data_parallel_incomm = parallel_driver_multiprocessing(sim)
+    print("Comparing results...")
+    for key, val in data_parallel.data_dict.items():
+        if isinstance(val, np.ndarray):
+            assert np.allclose(val, data_parallel_incomm.data_dict[key])
+            # assert np.allclose(val, data_parallel_mpi.data_dict[key])
+    print("results match!")
+    return
+
 
 if __name__ == "__main__":
     test_drivers_spinboson()
+    test_incommensurate_batch_size_serial()
+    test_incommensurate_batch_size_multiprocessing()
