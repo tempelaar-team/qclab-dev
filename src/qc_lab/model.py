1865ec9207659900da9062198d4c06dd0b93a30f
akrotz <alexkrotz@gmail.com>
2025-09-06 08:55:33 -0500

    Updated model class documentation.

--- src/qc_lab/model.py
+++ src/qc_lab/model.py
@@ -8,8 +8,7 @@ from qc_lab.constants import Constants
 
 class Model:
     """
-    The Model object provides the framework for defining the default constants of a
-    model and retrieving the ingredients of the model.
+    Model class for defining model constants and ingredients.
     """
 
     def __init__(self, default_constants=None, constants=None):
@@ -17,12 +16,21 @@ class Model:
             constants = {}
         if default_constants is None:
             default_constants = {}
+        # Merge default constants with user-provided constants.
         constants = {**default_constants, **constants}
+        # Construct a Constants object to hold constants.
+        # Provide an initialization function to reinitialize
+        # internal model constants upon changing any constant.
         self.constants = Constants(self.initialize_constants)
+        # Put constants from the dictionary into the Constants object.
         for key, val in constants.items():
             setattr(self.constants, key, val)
+        # Mark the constants as initialized.
         self.constants._init_complete = True
+        # Copy the ingredients to ensure they are not shared across instances.
         self.ingredients = copy.deepcopy(self.ingredients)
+        # Flags to indicate if the quantum Hamiltonian and quantum-classical
+        # gradients need to be updated.
         self.update_h_q = True
         self.update_dh_qc_dzc = True
 
@@ -45,6 +53,7 @@ class Model:
     def initialize_constants(self):
         """
         Initialize the constants for the model and ingredients.
+        Calls ingredients with names starting with "_init_".
         """
         for ingredient in self.ingredients[::-1]:
             if ingredient[0].startswith("_init_") and ingredient[1] is not None:
