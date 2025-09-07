3274d10bf64c4a6b4cdeee3ee64b863292a2e233
akrotz <alexkrotz@gmail.com>
2025-09-07 15:37:35 -0500

    Added output test for gauge fixing in FSSH.

--- tests/test_output.py
+++ tests/test_output.py
@@ -178,7 +178,64 @@ def test_output_different_h():
                 )
     return
 
+
+def test_output_fssh_gauge_fixing():
+    """
+    This test ensures that the initial choice of phase in FSSH does not affect
+    the final result of a calculation. 
+
+    The test proceeds by adding a random phase to the eigenvectors computed at the
+    outset of the simulation. The relative phases of these vectors are fixed so that 
+    the derivative couplings are real-valued ("phase_der_couple"). At subsequent 
+    timesteps, the relative phase of the new eigenvectors is determined from maximizing the
+    real part of their overlap with the previous set of eigenvectors ("phase_overlap"). 
+     
+    """
+    local_settings = {"num_trajs":10, "batch_size":10}
+    reference_folder = os.path.join(os.path.dirname(__file__), "reference/")
+    my_FSSH = FewestSwitchesSurfaceHopping({"gauge_fixing":"phase_overlap"})
+    def add_random_phase(algorithm, sim, parameters, state):
+        np.random.seed(10)
+        random_phases = np.exp(
+            1j * 2.0 * np.pi * np.random.rand(sim.settings.batch_size, sim.model.constants.num_quantum_states)
+        )
+        state.eigvecs = random_phases[:, np.newaxis, :] * state.eigvecs
+        return parameters, state
+    my_FSSH.initialization_recipe.insert(5, add_random_phase)
+    for model_class in [
+        SpinBoson,
+        HolsteinLattice,
+        KSpaceHolsteinLattice,
+        FMOComplex,
+        TullyProblemOne,
+        TullyProblemTwo,
+        TullyProblemThree,
+    ]:
+        for algorithm_class in [my_FSSH]:
+            print(f"Testing {model_class.__name__} with FewestSwitchesSurfaceHopping")
+            sim = Simulation({**model_sim_settings[model_class.__name__], **local_settings})
+            model_name = model_class.__name__
+            algorithm_name = "FewestSwitchesSurfaceHopping"
+            sim.model = model_class()
+            sim.model.initialize_constants()
+            sim.algorithm = algorithm_class
+            sim.state.wf_db = np.zeros(
+                sim.model.constants.num_quantum_states, dtype=complex
+            )
+            sim.state.wf_db[0] = 1j
+            data = serial_driver(sim)
+            data_correct = Data().load(
+                os.path.join(reference_folder, f"{model_name}_{algorithm_name}.h5")
+            )
+            for key, val in data.data_dict.items():
+                print('Comparing ', key)
+                np.testing.assert_allclose(
+                    val, data_correct.data_dict[key], rtol=1e-5, atol=1e-8
+                )
+    return
+
 if __name__ == "__main__":
     test_output_serial()
     test_output_multiprocessing()
     test_output_different_h()
+    test_output_fssh_gauge_fixing()
