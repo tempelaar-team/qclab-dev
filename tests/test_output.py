1bb385f72666684259a48478cedec651ca0d280e
akrotz <alexkrotz@gmail.com>
2025-09-28 22:04:07 -0500

    updated test data and fixed model specific constants.

--- tests/test_output.py
+++ tests/test_output.py
@@ -18,6 +18,7 @@ from qc_lab.models import (
 )
 from qc_lab.algorithms import MeanField, FewestSwitchesSurfaceHopping
 from qc_lab.dynamics import serial_driver, parallel_driver_multiprocessing
+from qc_lab.numerical_constants import INVCM_TO_300K
 
 
 model_sim_settings = {
@@ -66,8 +67,77 @@ model_sim_settings = {
 }
 
 
+model_settings = {
+    "SpinBoson": {
+        "kBT": 1.0,
+        "V": 0.5,
+        "E": 0.5,
+        "A": 100,
+        "W": 0.1,
+        "l_reorg": 0.005,
+        "boson_mass": 1.0,
+    },
+    "HolsteinLattice": {
+        "kBT": 1.0,
+        "g": 0.5,
+        "w": 0.5,
+        "N": 10,
+        "J": 1.0,
+        "phonon_mass": 1.0,
+        "periodic": True,
+    },
+    "HolsteinLatticeReciprocalSpace": {
+        "kBT": 1.0,
+        "g": 0.5,
+        "w": 0.5,
+        "N": 10,
+        "J": 1.0,
+        "phonon_mass": 1.0,
+        "periodic": True,
+    },
+    "FMOComplex": {
+        "kBT": 1.0,
+        "mass": 1.0,
+        "l_reorg": 35.0 * INVCM_TO_300K,  # reorganization energy
+        "w_c": 106.14 * INVCM_TO_300K,  # characteristic frequency
+        "N": 200,
+    },
+    "TullyProblemOne": {
+        "init_momentum": 10.0,
+        "init_position": -5.0,
+        "mass": 2000.0,
+        "A": 0.01,
+        "B": 1.6,
+        "C": 0.005,
+        "D": 1.0,
+    },
+    "TullyProblemTwo": {
+        "init_momentum": 10.0,
+        "init_position": -5.0,
+        "mass": 2000.0,
+        "A": 0.1,
+        "B": 0.28,
+        "C": 0.015,
+        "D": 0.06,
+        "E_0": 0.05,
+    },
+    "TullyProblemThree": {
+        "init_momentum": 10.0,
+        "init_position": -5.0,
+        "mass": 2000.0,
+        "A": 0.0006,
+        "B": 0.1,
+        "C": 0.9,
+    },
+}
+
+
+
 def test_output_serial():
-    local_settings = {"num_trajs":10, "batch_size":10}
+    """
+    This test verifies that the serial driver produces the expected results.
+    """
+    local_settings = {"num_trajs": 10, "batch_size": 10}
     reference_folder = os.path.join(os.path.dirname(__file__), "reference/")
     for model_class in [
         SpinBoson,
@@ -81,10 +151,12 @@ def test_output_serial():
         for algorithm_class in [MeanField, FewestSwitchesSurfaceHopping]:
             print(f"Testing {model_class.__name__} with {algorithm_class.__name__}")
 
-            sim = Simulation({**model_sim_settings[model_class.__name__], **local_settings})
+            sim = Simulation(
+                {**model_sim_settings[model_class.__name__], **local_settings}
+            )
             model_name = model_class.__name__
             algorithm_name = algorithm_class.__name__
-            sim.model = model_class()
+            sim.model = model_class(model_settings[model_class.__name__])
             sim.model.initialize_constants()
             sim.algorithm = algorithm_class()
             sim.state.wf_db = np.zeros(
@@ -103,7 +175,11 @@ def test_output_serial():
 
 
 def test_output_multiprocessing():
-    local_settings = {"num_trajs":10, "batch_size":5}
+    """
+    This test verifies that the multiprocessing driver produces the same results
+    as the serial driver.
+    """
+    local_settings = {"num_trajs": 10, "batch_size": 5}
     reference_folder = os.path.join(os.path.dirname(__file__), "reference/")
     for model_class in [
         SpinBoson,
@@ -117,10 +193,12 @@ def test_output_multiprocessing():
         for algorithm_class in [MeanField, FewestSwitchesSurfaceHopping]:
             print(f"Testing {model_class.__name__} with {algorithm_class.__name__}")
 
-            sim = Simulation({**model_sim_settings[model_class.__name__], **local_settings})
+            sim = Simulation(
+                {**model_sim_settings[model_class.__name__], **local_settings}
+            )
             model_name = model_class.__name__
             algorithm_name = algorithm_class.__name__
-            sim.model = model_class()
+            sim.model = model_class(model_settings[model_class.__name__])
             sim.model.initialize_constants()
             sim.algorithm = algorithm_class()
             sim.state.wf_db = np.zeros(
@@ -139,7 +217,11 @@ def test_output_multiprocessing():
 
 
 def test_output_different_h():
-    local_settings = {"num_trajs":10, "batch_size":10}
+    """
+    This test ensures that changing the value of the classical_coordinate_weight does 
+    not change the result of the simulation.
+    """
+    local_settings = {"num_trajs": 10, "batch_size": 10}
     reference_folder = os.path.join(os.path.dirname(__file__), "reference/")
     for model_class in [
         SpinBoson,
@@ -153,16 +235,18 @@ def test_output_different_h():
         for algorithm_class in [MeanField, FewestSwitchesSurfaceHopping]:
             print(f"Testing {model_class.__name__} with {algorithm_class.__name__}")
 
-            sim = Simulation({**model_sim_settings[model_class.__name__], **local_settings})
+            sim = Simulation(
+                {**model_sim_settings[model_class.__name__], **local_settings}
+            )
             model_name = model_class.__name__
             algorithm_name = algorithm_class.__name__
-            sim.model = model_class()
+            sim.model = model_class(model_settings[model_class.__name__])
             sim.model.initialize_constants()
             np.random.seed(10)
             # Adjust the weights to be a random positive number between 1 and 2.
-            sim.model.constants.classical_coordinate_weight = np.random.rand(
-                sim.model.constants.num_classical_coordinates
-            ) + 1.0
+            sim.model.constants.classical_coordinate_weight = (
+                np.random.rand(sim.model.constants.num_classical_coordinates) + 1.0
+            )
             sim.algorithm = algorithm_class()
             sim.state.wf_db = np.zeros(
                 sim.model.constants.num_quantum_states, dtype=complex
@@ -182,25 +266,32 @@ def test_output_different_h():
 def test_output_fssh_gauge_fixing():
     """
     This test ensures that the initial choice of phase in FSSH does not affect
-    the final result of a calculation. 
+    the final result of a calculation.
 
     The test proceeds by adding a random phase to the eigenvectors computed at the
-    outset of the simulation. The relative phases of these vectors are fixed so that 
-    the derivative couplings are real-valued ("phase_der_couple"). At subsequent 
+    outset of the simulation. The relative phases of these vectors are fixed so that
+    the derivative couplings are real-valued ("phase_der_couple"). At subsequent
     timesteps, the relative phase of the new eigenvectors is determined from maximizing the
-    real part of their overlap with the previous set of eigenvectors ("phase_overlap"). 
-     
+    real part of their overlap with the previous set of eigenvectors ("phase_overlap").
+
     """
-    local_settings = {"num_trajs":10, "batch_size":10}
+    local_settings = {"num_trajs": 10, "batch_size": 10}
     reference_folder = os.path.join(os.path.dirname(__file__), "reference/")
-    my_FSSH = FewestSwitchesSurfaceHopping({"gauge_fixing":"phase_overlap"})
+    my_FSSH = FewestSwitchesSurfaceHopping({"gauge_fixing": "phase_overlap"})
+
     def add_random_phase(algorithm, sim, parameters, state):
         np.random.seed(10)
         random_phases = np.exp(
-            1j * 2.0 * np.pi * np.random.rand(sim.settings.batch_size, sim.model.constants.num_quantum_states)
+            1j
+            * 2.0
+            * np.pi
+            * np.random.rand(
+                sim.settings.batch_size, sim.model.constants.num_quantum_states
+            )
         )
         state.eigvecs = random_phases[:, np.newaxis, :] * state.eigvecs
         return parameters, state
+
     my_FSSH.initialization_recipe.insert(5, add_random_phase)
     for model_class in [
         SpinBoson,
@@ -213,10 +304,12 @@ def test_output_fssh_gauge_fixing():
     ]:
         for algorithm_class in [my_FSSH]:
             print(f"Testing {model_class.__name__} with FewestSwitchesSurfaceHopping")
-            sim = Simulation({**model_sim_settings[model_class.__name__], **local_settings})
+            sim = Simulation(
+                {**model_sim_settings[model_class.__name__], **local_settings}
+            )
             model_name = model_class.__name__
             algorithm_name = "FewestSwitchesSurfaceHopping"
-            sim.model = model_class()
+            sim.model = model_class(model_settings[model_class.__name__])
             sim.model.initialize_constants()
             sim.algorithm = algorithm_class
             sim.state.wf_db = np.zeros(
@@ -228,11 +321,55 @@ def test_output_fssh_gauge_fixing():
                 os.path.join(reference_folder, f"{model_name}_{algorithm_name}.h5")
             )
             for key, val in data.data_dict.items():
-                print('Comparing ', key)
+                print("Comparing ", key)
                 np.testing.assert_allclose(
                     val, data_correct.data_dict[key], rtol=1e-5, atol=1e-8
                 )
     return
+
+
+def test_output_fssh_deterministic():
+    """
+    This test verifies the deterministic version of FSSH.
+    """
+    local_settings = {"num_trajs": 10, "batch_size": 10}
+    reference_folder = os.path.join(os.path.dirname(__file__), "reference/")
+    for model_class in [
+        SpinBoson,
+        HolsteinLattice,
+        HolsteinLatticeReciprocalSpace,
+        FMOComplex,
+        TullyProblemOne,
+        TullyProblemTwo,
+        TullyProblemThree,
+    ]:
+        algorithm_class = FewestSwitchesSurfaceHopping
+        print(f"Testing {model_class.__name__} with deterministic {algorithm_class.__name__}")
+
+        sim = Simulation({**model_sim_settings[model_class.__name__], **local_settings})
+        model_name = model_class.__name__
+        algorithm_name = algorithm_class.__name__
+        sim.model = model_class(model_settings[model_class.__name__])
+        sim.model.initialize_constants()
+        sim.settings.batch_size *= sim.model.constants.num_quantum_states
+        sim.settings.num_trajs *= sim.model.constants.num_quantum_states
+        sim.algorithm = algorithm_class({"fssh_deterministic": True})
+        sim.state.wf_db = np.zeros(
+            sim.model.constants.num_quantum_states, dtype=complex
+        )
+        sim.state.wf_db[0] = 1j
+        data = serial_driver(sim)
+        data_correct = Data().load(
+            os.path.join(
+                reference_folder, f"{model_name}_{algorithm_name}+'_deterministic.h5"
+            )
+        )
+        for key, val in data.data_dict.items():
+            np.testing.assert_allclose(
+                val, data_correct.data_dict[key], rtol=1e-5, atol=1e-8
+            )
+    return
+
 
 if __name__ == "__main__":
     test_output_serial()
