88b716897cc9b64b85a656131fd821f0ca9f72b7
akrotz <alexkrotz@gmail.com>
2025-10-06 11:50:49 -0500

    Updated get attr of model class. Returns False, None if ingredient name is found but function is None.

--- src/qc_lab/model.py
+++ src/qc_lab/model.py
@@ -46,8 +46,10 @@ class Model:
             not found) and a flag indicating whether it exists.
         """
         for ingredient in self.ingredients[::-1]:
-            if ingredient[0] == ingredient_name and ingredient[1] is not None:
+            if ingredient[0] == ingredient_name and not(ingredient[1] is None):
                 return ingredient[1], True
+            if ingredient[0] == ingredient_name and (ingredient[1] is None):
+                return None, False
         return None, False
 
     def initialize_constants(self):
