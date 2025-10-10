8181f77d371a3e20a6e836f21c097776ee360b93
akrotz <alexkrotz@gmail.com>
2025-10-09 22:53:10 -0500

    Updated documentation and docstrings.

--- docs/user_guide/data.rst
+++ docs/user_guide/data.rst
@@ -7,7 +7,7 @@ Data
 Data Objects
 ---------------------------
 
-Data objects are instances of the `qclab.data.Data` class and are used to store and manage the results of a simulation. They provide methods for collecting, processing, and saving data, as well as logging errors or warnings during a simulation.
+Data objects are instances of the ``qclab.data.Data`` class and are used to store and manage the results of a simulation. They provide methods for collecting, processing, and saving data, as well as logging errors or warnings during a simulation.
 
 In general, a data object has the following attributes:
 
@@ -19,3 +19,35 @@ Data objects provide several methods for managing and processing the data they c
 - ``add_data``: adds data from an existing data object to the current one.
 - ``save``: saves the data object to a file.
 - ``load``: loads a data object from a file (this adds to any existing data).
+
+These methods are documented here:
+
+.. autofunction:: qclab.data.Data.add_data
+.. autofunction:: qclab.data.Data.save
+.. autofunction:: qclab.data.Data.load
+
+
+Example
+---------------------------
+
+Here is a simple example of running a simulation and plotting from the data object returned by the driver:
+
+.. code-block:: python
+
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from qclab import Simulation
+    from qclab.models import SpinBoson
+    from qclab.algorithms import MeanField
+    from qclab.dynamics import serial_driver
+    
+    sim = Simulation()
+    sim.model = SpinBoson()
+    sim.algorithm = MeanField()
+    sim.initial_state.wf_db= np.array([1,0], dtype=complex)
+    data = serial_driver(sim)
+
+    t = data.data_dict['t']
+    plt.plot(t, np.real(np.einsum('tii->ti',data.data_dict['dm_db'])))
+    plt.title('Diabatic populations')
+    plt.show()
--- src/qclab/data.py
+++ src/qclab/data.py
@@ -27,10 +27,9 @@ class Data:
 
     def add_output_to_data_dict(self, sim, state, t_ind):
         """
-        Add data to the output dictionary ``data_dict``.
+        Add data to the output dictionary ``self.data_dict``.
 
-        Args
-        ----
+        .. rubric:: Args
         sim: Simulation
             The simulation object containing settings and parameters.
         state: Variable
@@ -70,10 +69,9 @@ class Data:
 
     def add_data(self, new_data):
         """
-        Add data from ``new_data`` to the output dictionary ``data_dict``.
+        Add data from ``new_data`` to the output dictionary ``self.data_dict``.
 
-        Args
-        ----
+        .. rubric:: Args
         new_data: Data
             A Data instance containing the new data to merge.
         """
@@ -98,41 +96,42 @@ class Data:
         if getattr(new_data, "log", ""):
             self.log += new_data.log
 
-    def save(self, filename, DISABLE_H5PY=DISABLE_H5PY):
+    def save(self, filename, disable_h5py=DISABLE_H5PY):
         """
         Save the data to disk with file name ``filename``.
 
         If h5py is available the data is stored as an HDF5 archive; otherwise
         each variable is saved using numpy.savez.
 
-        Args
-        ----
+        .. rubric:: Args
         filename : str
             The file name to save the data to.
+        disable_h5py : bool, default: qclab.utils.DISABLE_H5PY
+            If True, h5py is not used even if available.
         """
-        if DISABLE_H5PY:
+        if disable_h5py:
             np.savez(filename, log=self.log, **self.data_dict)
         else:
             with h5py.File(filename, "w") as h5file:
                 self._recursive_save(h5file, "/", self.data_dict)
                 h5file.attrs["log"] = self.log
 
-    def load(self, filename, DISABLE_H5PY=DISABLE_H5PY):
+    def load(self, filename, disable_h5py=DISABLE_H5PY):
         """
         Load a Data object from ``filename``.
 
-        Args
-        ----
+        .. rubric:: Args
         filename : str
             The file name to load the data from.
+        disable_h5py : bool, default: qclab.utils.DISABLE_H5PY
+            If True, h5py is not used even if available.
 
-        Returns
-        -------
+        .. rubric:: Returns
         Data : Data
             The loaded Data object.
         """
         new_data = Data()
-        if DISABLE_H5PY:
+        if disable_h5py:
             loaded = np.load(filename, allow_pickle=True)
             new_data.data_dict = {key: loaded[key] for key in loaded.files if key != "log"}
             new_data.log = str(loaded.get("log", ""))
@@ -148,8 +147,7 @@ class Data:
         """
         Recursively save dictionary contents to an HDF5 group.
 
-        Args
-        ----
+        .. rubric:: Args
         h5file : h5py.File
             The HDF5 file object to save data into.
         path : str
@@ -172,8 +170,7 @@ class Data:
         """
         Recursively load dictionary contents from an HDF5 group.
 
-        Args
-        ----
+        .. rubric:: Args
         h5file : h5py.File
             The HDF5 file object to load data from.
         path : str
