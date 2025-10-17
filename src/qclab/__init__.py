2fa045053bd77125901c945eae92c77d633438e1
akrotz <alexkrotz@gmail.com>
2025-10-16 22:09:50 -0500

    Added version.

--- src/qclab/__init__.py
+++ src/qclab/__init__.py
@@ -13,3 +13,13 @@ import qclab.algorithms
 import qclab.dynamics
 
 configure_memory_logger()
+
+try:
+    from importlib.metadata import version as _pkg_version
+except Exception:
+    from importlib_metadata import version as _pkg_version
+
+try:
+    __version__ = _pkg_version("qclab")
+except Exception:
+    __version__ = "0+unknown"
